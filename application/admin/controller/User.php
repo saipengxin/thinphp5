67ea1ef35cbaeaec3a8b7735bc64adef19b0e0da
saipengxin <saipengxin@163.com>
2021-11-02 23:53:46 +0800

    模行

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -2,13 +2,19 @@
 
 namespace app\admin\controller;
 use think\Controller;
+use think\Db;
 
 class User extends Controller {
     public function index()
     {
-        $this->assign('age','');
-        $this->assign('id',5);
-        define('APP_PATH','./public/index.php');
-        return $this->fetch();
+        $data = [];
+        for ($i=1; $i < 10; $i++) {
+            $data[] = [
+                'username'  =>  'user'.$i,
+                'password'  =>  md5('admin123'.$i),
+            ];
+        }
+        $res = Db::table('user')->insertAll($data);
+        dump($res);
     }
 }
